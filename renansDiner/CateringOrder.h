cb817aad84aee4107761e3b6ab4a9b71a2a41788
renanav <renan.avrahami@gmail.com>
2016-05-19 15:18:27 -0400

    Compose CateringOrder class, add properties, make required imports

---
+++ renansDiner/CateringOrder.h
@@ -0,0 +1,13 @@
+//
+//  CateringOrder.h
+//  renansDiner
+//
+//  Created by Renan Avrahami on 5/19/16.
+//  Copyright © 2016 Renan Avrahami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CateringOrder : NSObject
+
+@end
---
+++ renansDiner/CateringOrder.m
@@ -0,0 +1,13 @@
+//
+//  CateringOrder.m
+//  renansDiner
+//
+//  Created by Renan Avrahami on 5/19/16.
+//  Copyright © 2016 Renan Avrahami. All rights reserved.
+//
+
+#import "CateringOrder.h"
+
+@implementation CateringOrder
+
+@end
